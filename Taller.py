be73981132ab3e61d24503cced5a7716e744ef70
Shir3n <ignacio.salazar.lopez@gmail.com>
2025-04-14 14:11:19 -0400

    commit2

---
+++ Taller.py
@@ -0,0 +1,22 @@
+#ejercicio 1
+print("Ignacio Salazar\n25")
+
+#ejercicio 2
+print("Breaking the habit")
+
+#ejercicio 3 
+print("૮ ˶ᵔ ᵕ ᵔ˶ ა")
+
+#ejercicio 4
+print("5")
+print("4")
+print("3")
+print("2")
+print("1")
+print("⋆✴︎˚｡⋆⋆✴︎˚｡⋆")
+
+print("BOOOM")
+print("⋆✴︎˚｡⋆⋆✴︎˚｡⋆")
+
+#ejercicio 5
+print("Memories COnsume\nLike opening the Wound\nIm picking me apart again")
